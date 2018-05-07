cd5d49c7503dfb6a456e6b63ad0aa5b8d32c75e6
josphat <josphatmacharia@gmail.com>
2018-05-07 17:22:17 +0300

    wrongly renamed flatten.js as maxprofit.js. Rectifying

--- maxprofit.js
+++ maxprofit.js
@@ -1,32 +1,65 @@
-//Array to flatten
-let arr = [1,2,[3,4,[5,6],7,[8,9]]];
-//array to store flattened array values
-let flat_arr = []; 
-//index of the the element to be added to flattened arrya
-let global_index = 0;
+#!/usr/bin/nodejs --harmony
 
-//function to do the actual flattening 
-//param: array to be flattened
-function flatten(arr){
-	//loop throught the input array
-	for (let index of arr){
-		//check if array value data type is a number or object
-		//and recurse if the value is of type object
-		if (typeof(arr[index]) == "number")
-			{
-			flat_arr[global_index]=arr[index];
-			global_index++;
-			}
-		else if (typeof(arr[index]) == "object")
-			{
-				flatten(arr[index]);
-			}
-
-	}
+//get array from arguments and create a JSON array
+let args = process.argv;
+let arr = JSON.parse(args[2]);
+//This function prints usage help
+function usage()
+{
+    script_path = args[1].split('/');
+	scriptname = script_path[script_path.length-1];
+	console.log("Usage: "+scriptname+" array\nExample: "+scriptname+" [2,6,3,5,8,1]");
 }
 
-//calling the flattening function
-flatten(arr);
+//function: maxprofit
+//param: array of integers representing stock prices
+function maxprofit(arr){
+	//check the arguments
+	if (process.argv.length != 3)
+		{
+		usage();
+		return 1;
+		}
+	//if array has single number print zero
+	if (arr.length == 1)
+		{
+		console.log(0);
+		}
+	//if array has two numbers compare them and print appropriate result
+	if (arr.length == 2)
+		{
+			if(arr[0] > arr[1])
+				console.log(-1);
+			else if(arr[0] == arr[1])
+				console.log(0);
+			else if(arr[0] < arr[1])
+				console.log(arr[1]-arr[0])
 
-//print the resulting flattened array
-console.log("Result: "+flat_arr);
+		}
+	//where arguments are greater than two, compare and print appropriate result
+	if (arr.length>2)
+	{
+		 let minbuyprice = arr[0];
+		 let minbuyday = 0;
+		 let maxsellprice = arr[0];
+		 let maxsellday = 0;
+		 for (day=0;day<(arr.length-1);day++)
+		 	{
+			if(arr[day] > maxsellprice)
+				{
+					maxsellprice = arr[day];
+					maxsellday = day;
+				}
+			if(arr[day] < minbuyprice)
+				{
+					minbuyprice = arr[day];
+					minbuyday = day;
+				}
+			}
+		 if(maxsellday == 0)
+		 	console.log(-1);
+		 else
+		  	console.log(maxsellprice-minbuyprice);
+	}
+}
+maxprofit(arr)
